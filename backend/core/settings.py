bd220ab21bb6444b2f18ab88b03e51dc43d93199
moomensaleem135 <moomensaleem135@gmail.com>
2024-07-22 19:00:25 +0500

    Update settings.py

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -160,13 +160,13 @@ CSRF_TRUSTED_ORIGINS = [
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:9000",
-    "http://localhost:3000",
-    "http://localhost:7035",
-    "https://specsources.vercel.app",
-    "*",
-]
+# CORS_ALLOWED_ORIGINS = [
+#     "http://127.0.0.1:9000",
+#     "http://localhost:3000",
+#     "http://localhost:7035",
+#     "https://specsources.vercel.app",
+#     "*",
+# ]
 
 CORS_ALLOW_HEADERS = [
     "accept",
@@ -178,5 +178,4 @@ CORS_ALLOW_HEADERS = [
     "user-agent",
     "x-csrftoken",
     "x-requested-with",
-    "*",
 ]
